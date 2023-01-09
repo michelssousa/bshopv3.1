b0281384a1fc111ff8ed0bedcb9e0db75a3ea523
Michel Sousa <michelsousa7@gmail.com>
2023-01-09 21:00:54 +0000

    Update 19 files

---
+++ packages/db/seed.ts
@@ -0,0 +1,39 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient({
+  log: ['query', 'error', 'warn'],
+})
+
+async function main() {
+  const admin = await prisma.user.upsert({
+    where: { email: 'alice@prisma.io' },
+    update: {},
+    create: {
+      email: 'admin@acme.com',
+      name: 'Admin',
+    },
+  })
+  const posts = await prisma.post.createMany({
+    data: [
+      {
+        title: 'Hello World',
+        content: 'This is my first post!',
+      },
+      {
+        title: 'Second Post',
+        content: 'This is my second post!',
+      },
+    ],
+  })
+  console.log({ admin, posts })
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async e => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
